fec769d4525a7bf89f7aacb35edeb3c600ea2177
Saiful islam <saifislam2167@gmail.com>
2018-10-17 15:23:56 +0200

    Demo with attachment

--- src/main/java/com/saif/simplemail/demo/DemoApp.java
+++ src/main/java/com/saif/simplemail/demo/DemoApp.java
@@ -2,6 +2,10 @@ package com.saif.simplemail.demo;
 
 import com.saif.simplemail.EmailService;
 import com.saif.simplemail.MailData;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -13,7 +17,11 @@ public class DemoApp {
     public static void main(String[] args) {
         Properties prop = getProperties();
         EmailService service = EmailService.getMailService();
-        service.sendEmail(prop, new MailData("saiful.islam@printoscope.pl", "test sub", "Body.."));
+        //service.sendEmail(prop, new MailData("saiful.islam@printoscope.pl", "test sub", "Body.."));
+        File f1 = new File("file1.txt");
+        File f2 = new File("file2.pdf");
+        MailData md1 = new MailData("saiful.islam@printoscope.pl", "test sub", "Body..", Arrays.asList(f1, f2));     
+        service.sendEmail(prop,md1);
     }
 
     public static Properties getProperties() {
